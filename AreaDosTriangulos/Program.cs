644f37daa8a3bd122ab8ac0007a659be49cae72f
DeveloperMatheus97 <matheuscaitanosouza18@outlook.com>
2022-05-04 11:04:23 -0300

    first commit

---
+++ AreaDosTriangulos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Praticar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double xA, xB, xC, yA, yB, yC;
+
+            Console.WriteLine("ENTRE COM AS MEDIDAS DO TRIANGULO X:");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("ENTRE COM AS MEDIDAS DO TRIANGULO Y:");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            p = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine("ÁREA DE X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("ÁREA DE Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("MAIOR ÁREA: X");
+            }
+            else
+            {
+                Console.WriteLine("MAIOR ÁREA: Y");
+            }
+
+        }
+    }
+}
